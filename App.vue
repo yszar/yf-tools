5177c40f3f5776e389cf94e8e278de19fdec3649
yszar <yszar@vip.qq.com>
2022-09-15 23:00:49 +0800

    chore: 初始化

---
+++ App.vue
@@ -0,0 +1,36 @@
+<script>
+export default {
+    onLaunch: function() {
+        console.warn(
+            '当前组件仅支持 uni_modules 目录结构 ，请升级 HBuilderX 到 3.1.0 版本以上！'
+        );
+        console.log('App Launch');
+    },
+    onShow: function() {
+        console.log('App Show');
+    },
+    onHide: function() {
+        console.log('App Hide');
+    }
+};
+</script>
+
+<style lang="scss">
+/*每个页面公共css */
+@import '@/uni_modules/uni-scss/index.scss';
+/* uni.css - 通用组件、模板样式库，可以当作一套ui库应用 */
+@import '/common/uni.css';
+/* #ifndef APP-NVUE */
+// @import '@/static/customicons.css';
+// 设置整个项目的背景色
+page {
+    background-color: $uni-bg-color;
+}
+
+/* #endif */
+.example-info {
+    font-size: 14px;
+    color: #333;
+    padding: 10px;
+}
+</style>
---
+++ pages/index/index.vue
@@ -0,0 +1,24 @@
+<template>
+    <view>
+        <uni-group mode="card">
+            <view class="test">分组内容</view>
+            <view>分组内容</view>
+            <view>分组内容</view>
+            <view>分组内容</view>
+        </uni-group>
+    </view>
+</template>
+
+<script>
+export default {
+    data() {
+        return {};
+    }
+};
+</script>
+
+<style lang="scss">
+.test {
+    background-color: $uni-bg-color;
+}
+</style>
